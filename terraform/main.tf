98c82cc18f3ffc0cd42ecf165fab88c542c5f49f
Zeeshan Ahsan <zeeshan.ahsan15@gmail.com>
2024-06-12 17:12:09 -0400

    add security_grp block with ingress and egress

--- terraform/main.tf
+++ terraform/main.tf
@@ -33,6 +33,48 @@ resource "aws_instance" "servernode" {
     }
 }
 
+resource "aws_security_group" "maingroup" {
+    egress = [
+        {
+            cidr_blocks = ["0.0.0.0/0"]
+            description = ""
+            from_port = 0
+            ipv6_cidr_blocks = []
+            prefix_list_ids = []
+            protocol = "-1"
+            security_groups = []
+            self = false
+            to_port = 0
+        }
+    ]
+
+    ingress = [
+        {
+            cidr_blocks = ["0.0.0.0/0"]
+            description = ""
+            from_port = 22
+            ipv6_cidr_blocks = []
+            prefix_list_ids = []
+            protocol = "tcp"
+            security_groups = []
+            self = false
+            to_port = 22
+        },
+        {
+            cidr_blocks = ["0.0.0.0/0"]
+            description = ""
+            from_port = 80
+            ipv6_cidr_blocks = []
+            prefix_list_ids = []
+            protocol = "tcp"
+            security_groups = []
+            self = false
+            to_port = 80
+        }
+    ]
+ 
+}
+
 resource "aws_key_pair" "deployer"{
     key_name = var.key_name
     public_key = var.public_key
