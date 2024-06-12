8640172c550b9e6cb7d5fd60357ef137794d4aa3
Zeeshan Ahsan <zeeshan.ahsan15@gmail.com>
2024-06-12 17:03:16 -0400

    add aws-instance create block

--- terraform/main.tf
+++ terraform/main.tf
@@ -12,3 +12,30 @@ terraform {
     }
 }
 
+provider "aws" {
+  region = var.region
+}
+
+resource "aws_instance" "name" {
+    ami = "ami-0f30a9c3a48f3fa79"
+    instance_type = "t2.micro"
+    #Attach aws_key_pair to this instance
+    key_name = aws_key_pair.deployer.key_name
+    connection {
+        type = "ssh"
+        host = self.public_ip
+        user = "ubuntu"
+        private_key = var.private_key
+        timeout = "4m"
+    }
+    tags = {
+        "name" = "DeployVM"
+    }
+}
+
+resource "aws_key_pair" "deployer"{
+    key_name = var.key_name
+    public_key = var.public_key
+
+}
+
